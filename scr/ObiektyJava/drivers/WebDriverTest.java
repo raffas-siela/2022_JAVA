587ff2eabaea38bd2466eb3c5e00bc6a348b20f9
raffas siela <raffas.siela@gmail.com>
2022-10-19 21:35:29 +0200

    commit01, 19 paździenrik

--- scr/ObiektyJava/drivers/WebDriverTest.java
+++ scr/ObiektyJava/drivers/WebDriverTest.java
@@ -1,28 +1,41 @@
 package ObiektyJava.drivers;
 
+import zadania.drivers.ChromeDriver;
+import zadania.drivers.FirefoxDriver;
 import zadania.drivers.WebDriver;
 
 //modyfikacja WebDriverTest aby skorzystać z poliformizmu
-//abyśmy nie musieli dupkować kodu
+//abyśmy nie musieli duplikować kodu
+//chcemy uruchomić kod dla 2 przeglądarek
 
 public class WebDriverTest {
     public static void main(String[] args) {
 
-        WebDriver driver = new ChromeDriver();
-        chromeDriver.get();
-        chromeDriver.findElementBy();
-        chromeDriver.findElementBy();
-        chromeDriver.findElementBy();
-        chromeDriver.findElementBy();
-        chromeDriver.findElementBy();
-
-        FirefoxDriver firefoxDriver = new FirefoxDriver();
-        firefoxDriver.get();
-        firefoxDriver.findElementBy();
-        firefoxDriver.findElementBy();
-        firefoxDriver.findElementBy();
-        firefoxDriver.findElementBy();
-        firefoxDriver.findElementBy();
+        //chcemy zedfiniować, że nasz Driver jest jakąś przeglądarką, czyli jest typu WebDriver
+        //tworzymy metodę getDriver, która będzie pobierała drivera
+        // w zależności od parametru zostanie zwrócony forefox lub chrome
+        //nie mamy metody getDriver,
+        // ale pomoc intelij, tworzymy automatycznie metodę jak podkresli się na czerwono jej brak
+        WebDriver driver = getDriver("chrome");
+        driver.get();
+        driver.findElementBy();
+        driver.findElementBy();
+        driver.findElementBy();
+        driver.findElementBy();
+        driver.findElementBy();
 
     }
+//intelij utworzył metodę getDriver
+    //musimy tylko uzupełnić String name zamiast Chrome
+    private static WebDriver getDriver(String name) {
+//dostarczamy implementację metody stworzonej przez intelij
+        //równość stringów sprawdzamy przez name.equals("chrome")
+        if (name.equals("chrome")) {
+            return new ChromeDriver();
+        } else if (name.equals("firefox")) {
+            return new FirefoxDriver();
+        }
+        return null;
+    }
 }
+
